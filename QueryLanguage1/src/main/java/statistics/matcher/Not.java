5d360a53367cba9e4a75f1c0c07d1ff32aa0aa3e
ndjurabe <nargiza.djurabekova@helsinki.fi>
2014-04-30 22:04:41 +0300

    ex5

---
+++ QueryLanguage1/src/main/java/statistics/matcher/Not.java
@@ -0,0 +1,32 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package statistics.matcher;
+
+import statistics.Player;
+
+/**
+ *
+ * @author nargiza
+ */
+public class Not implements Matcher{
+    
+ private Matcher[] matchers;
+
+    public Not(Matcher... matchers) {
+        this.matchers = matchers;
+    }
+
+    @Override
+    public boolean matches(Player p) {
+        for (Matcher matcher : matchers) {
+            if (!matcher.matches(p)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+    
+}
---
+++ QueryLanguage1/src/main/java/statistics/matcher/Or.java
@@ -0,0 +1,32 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package statistics.matcher;
+
+import statistics.Player;
+
+/**
+ *
+ * @author nargiza
+ */
+public class Or implements Matcher{
+    
+ private Matcher[] matchers;
+
+    public Or(Matcher... matchers) {
+        this.matchers = matchers;
+    }
+
+    @Override
+    public boolean matches(Player p) {
+        for (Matcher matcher : matchers) {
+            if (matcher.matches(p)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+    
+}
